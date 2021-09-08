7c8a3c13a05cfd1e2019d9a5abdc59b588d30522
JuanC-358 <juanc358a@gmail.com>
2021-09-07 22:25:52 -0500

    fix2

--- Procfile
+++ Procfile
@@ -1,2 +1,2 @@
-web: java $JAVA_OPTS -cp target/classes:target/dependency/* edu.escuelaing.nanosparkweb.NanoSparkServer
+web: java $JAVA_OPTS -cp target/classes:target/dependency/* edu.escuelaing.nanosparkweb.NanoSparkWebDemo
 
--- src/test/java/edu/escuelaing/AppTest.java
+++ src/test/java/edu/escuelaing/AppTest.java
@@ -33,7 +33,7 @@ public class AppTest extends TestCase {
 
     public void testDeberiaEncontrarJS() {
         try {
-            String urlString = "";
+            String urlString = "https://arepclientesyservicios.herokuapp.com/";
             BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(new URL(urlString + "/webapp.js").openStream()));
             assertTrue(true);
         } catch (IOException e) {
@@ -43,7 +43,7 @@ public class AppTest extends TestCase {
 
     public void testDeberiaEncontrarImg() {
         try {
-            String urlString = "";
+            String urlString = "https://arepclientesyservicios.herokuapp.com/";
             BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(new URL(urlString + "/Kri.jpg").openStream()));
             assertTrue(true);
         } catch (IOException e) {
@@ -53,7 +53,7 @@ public class AppTest extends TestCase {
 
     public void testDeberiaEncontrarHTML() {
         try {
-            String urlString = "";
+            String urlString = "https://arepclientesyservicios.herokuapp.com/";
             BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(new URL(urlString + "/index.html").openStream()));
             assertTrue(true);
         } catch (IOException e) {
